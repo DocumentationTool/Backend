173ee05c054973179ac86b84a90a3c1761fcc743
Wonkglorg <Wonkglorg.18@gmail.com>
2025-03-26 21:37:29 +0100

    Changed tests to random free port

--- Doc-Api/src/test/java/com/wonkglorg/doc/api/controller/BaseIntegrationTest.java
+++ Doc-Api/src/test/java/com/wonkglorg/doc/api/controller/BaseIntegrationTest.java
@@ -26,7 +26,7 @@ import java.util.Map;
 
 import static java.lang.Thread.sleep;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=8091")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BaseIntegrationTest {
 
     private static boolean deleteOnExit = false;
