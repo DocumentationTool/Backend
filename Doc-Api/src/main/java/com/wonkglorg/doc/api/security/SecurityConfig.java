bab88872bd696ff4c3a47f66d8c40cf1dd3ab9ba
Wonkglorg <Wonkglorg.18@gmail.com>
2025-04-01 19:41:07 +0200

    Fixed Configuration

--- Doc-Api/src/main/java/com/wonkglorg/doc/api/security/SecurityConfig.java
+++ Doc-Api/src/main/java/com/wonkglorg/doc/api/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -43,6 +44,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
+			.cors(Customizer.withDefaults())
 			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests(auth -> auth
 					.requestMatchers("/**").permitAll() // Allow all paths
